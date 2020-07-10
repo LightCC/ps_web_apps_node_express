d2ed181ddcbf686fe0eb5c6e980ccd3b980dc703
Mike Leiter <light_cc@yahoo.com>
2020-07-10 19:39:19 -0400

    Use eslint with the 'airbnb' opinionated rules, reformatting some code, etc.

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+/* eslint-disable no-console */
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 // morgan('combined') for a lot of http info to the console
 // morgan('tiny') for limited info
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public/')))
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
-})
+});
 
-app.listen(3000, function(){
+app.listen(3000, () => {
   console.log(`listening on port ${chalk.green('3000')}`);
 });
