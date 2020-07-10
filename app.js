92f249bed256e8faba9b5cb9b7c14c571a3698dd
Mike Leiter <light_cc@yahoo.com>
2020-07-10 00:12:07 -0400

    Serving static files from node_modules folders

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var app = express();
 // morgan('tiny') for limited info
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '/views/index.html'));
 })
