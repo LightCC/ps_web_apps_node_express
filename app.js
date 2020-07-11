067ded41dba1983fc7629e064c325f3b5fac6bf5
Mike Leiter <light_cc@yahoo.com>
2020-07-10 22:48:22 -0400

    Setup NodeMon

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-console */
 const express = require('express');
 const chalk = require('chalk');
+// eslint-disable-next-line no-unused-vars
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // morgan('combined') for a lot of http info to the console
 // morgan('tiny') for limited info
@@ -19,6 +21,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`listening on port ${chalk.green('3000')}`);
 });
