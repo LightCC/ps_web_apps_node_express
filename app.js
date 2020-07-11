9827f9bcff37e8aae8ed81956f9be785f1edd6cb
Mike Leiter <light_cc@yahoo.com>
2020-07-11 02:06:47 -0400

    Setup initial navigation (Books/Authors) and a route to /books and /books/single

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 // morgan('combined') for a lot of http info to the console
 // morgan('tiny') for limited info
@@ -19,8 +20,29 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('hello books!!');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book!!');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a1', 'b2'], title: 'Library', pagename: 'MyLibrary' });
+  res.render(
+    'index',
+    {
+      nav: [
+        { title: 'Books', link: '/books' },
+        { title: 'Authors', link: '/authors' }
+      ],
+      title: 'Library',
+      pagename: 'MyLibrary'
+    }
+  );
 });
 
 app.listen(port, () => {
