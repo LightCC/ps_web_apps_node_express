bc7cc3d8df775ca28b7c5179ed6e889a8faee279
Mike Leiter <light_cc@yahoo.com>
2020-07-12 01:29:15 -0400

    Middleware working to centralize db queries for each page

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 // Setup a router that we will use for all the `/books` routes for pages with book lists/info
 const bookRouter = express.Router();
 const sqlite3 = require('sqlite3').verbose();
-// const debug = require('debug')('app:bookRoutes');
+const debug = require('debug')('app:bookRoutes');
 
 const db = new sqlite3.Database('library.db3');
 
@@ -11,33 +11,45 @@ const db = new sqlite3.Database('library.db3');
 // so the below routes are appended to that (i.e. `/` is the same as `/books`)
 function router(nav) {
   bookRouter.route('/')
-    .get((req, res) => {
+    .all((req, res, next) => {
       db.all('select * from books', (err, records) => {
-        res.render(
-          'bookListView',
-          {
-            nav,
-            title: 'Book List',
-            pagename: 'MyLibrary - Books',
-            books: records
-          }
-        );
+        req.books = records;
+        debug('retreive books');
+        next();
       });
+    })
+    .get((req, res) => {
+      debug(req.books);
+      res.render(
+        'bookListView',
+        {
+          nav,
+          title: 'Book List',
+          pagename: 'MyLibrary - Books',
+          books: req.books
+        }
+      );
     });
-  bookRouter.route('/:id').get((req, res) => {
-    const { id } = req.params;
-    db.all(`select * from books where id = ${id}`, (err, record) => {
+  bookRouter.route('/:id')
+    .all((req, res, next) => {
+      const { id } = req.params;
+      db.all(`select * from books where id = ${id}`, (err, record) => {
+        debug(`retreive "${record[0].title}"`);
+        [req.book] = record;
+        next();
+      });
+    })
+    .get((req, res) => {
       res.render(
         'bookView',
         {
           nav,
           title: 'Book Info',
           pagename: 'MyLibrary - Book Info',
-          book: record[0]
+          book: req.book
         }
       );
     });
-  });
   return bookRouter;
 }
 
